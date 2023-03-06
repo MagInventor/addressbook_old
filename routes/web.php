8670634cfa5d77e4d8aa702d21c134547a1baec8
dreamtogostar <dreamerstargazer@yandex.ru>
2023-03-06 12:39:18 +0300

    feat: add maincontroller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+
+Route::get('/', 'MainController@index');
+
+Route::get('/about', function() {
+    return view('about');
 });
+
+Route::get('/{vue_capture?}', function() {
+    return view('home');
+})->where('vue_capture', '[\/\w\.-]*');
